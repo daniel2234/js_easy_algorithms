e79eae170922f716d54184af4caf17a7cec137b8
daniel2234 <kwiatkowski.daniel@gmail.com>
2021-01-12 21:28:16 -0500

    continuing algorithms

--- easy_3/cleanwords.js
+++ easy_3/cleanwords.js
@@ -1,13 +1,11 @@
 // Clean up the words
 // Given a string that consists of some words and an assortment of non-alphabetic characters, write a function that returns that string with all of the non-alphabetic characters replaced by spaces. If one or more non-alphabetic characters occur in a row, you should only have one space in the result (i.e., the result string should never have consecutive spaces).
 
-function cleanUp(sentence) {
-  let splitSentence = sentence.split('').filter(char => /[a-z\s]/.test(char));
-  console.log(splitSentence.join(''));
+// function cleanUp(sentence) {
+//   let splitSentence = sentence.split('').filter(char => /[a-z\s]/.test(char));
+//   console.log(splitSentence.join(''));
 
-}
-
-
+// }
 
 cleanUp("---what's my +*& line?");    // " what s my line "
 
